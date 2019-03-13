06caa8334be25924d44cc0c8bf3a2a6498efb410
unknown <felixjustspeter.hungershausen671@alu.uhu.es>
2019-03-13 17:28:59 +0100

    my first prgram

---
+++ manandwoman.pl
@@ -0,0 +1,26 @@
+%man(Person)
+%true if Person is a man.
+
+man(jose).
+man(sebastian).
+man(justus).
+man(kevin).
+man(teun).
+
+woman(lisanne).
+woman(wendele).
+woman(andrea).
+
+% person(P)
+% it is true if P is a man or a woman
+
+person(P):-man(P).
+person(P):-woman(P).
+
+
+% person(P) -> animal(P) and human(P)
+
+animal(P):-person(P).
+human(P):-person(P).
+
+% person(P) -> 
